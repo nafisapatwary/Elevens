ff38d952630f0d11e742387e02fc2d3ecee922e5
nafisapatwary <email@email.com>
2024-04-02 22:39:14 -0400

    finisheddddd

--- src/Deck.java
+++ src/Deck.java
@@ -1,24 +1,23 @@
 import java.util.*;
+import java.util.concurrent.LinkedBlockingDeque;
+
 public class Deck {
     private ArrayList<Card> deck;
     private ArrayList<Card> hand;
     public Deck(ArrayList<Card> hand){
         deck = Card.buildDeck();
         this.hand = hand;
-        for (int i = 0; i < hand.size(); i++){
-            Card current = hand.get(i);
-            for (int j = 0; j < deck.size(); j++){
-                if (deck.get(j).equals(current)){
-                    deck.remove(j);
-                    j--;
-                }
-            }
+        for (Card current : this.hand) {
+            //idk intelliJ told me to do this and it worked
+            deck.removeIf(x -> current.getValue().equals(x.getValue()) && current.getSuit().equals(x.getSuit()));
         }
     }
+
 
     public ArrayList<Card> getDeck(){
         return deck;
     }
+
     public Card getNewCard(){
         int randomCardIdx = (int)(Math.random() * deck.size());
         Card newCard = deck.get(randomCardIdx);
@@ -26,17 +25,64 @@ public class Deck {
         return newCard;
     }
 
-    public boolean canBeReplaced(){
-
+    public void removeCard(Card c){
+        deck.remove(c);
     }
-    public boolean noPossibleCombinationsLeft(){
-        for (int i = 0; i < hand.size(); i++){
-            for (int j = 1; j < hand.size(); j++){
 
+    public boolean canBeReplaced(ArrayList<Card> selected){
+        int total = 0;
+        boolean jack = false;
+        boolean queen = false;
+        boolean king = false;
+        for (Card c: selected){
+            if (c.getValue().equals("J") || c.getValue().equals("Q") || c.getValue().equals("K") || c.getValue().equals("A")){
+                if (c.getValue().equals("J")) jack = true;
+                if (c.getValue().equals("Q")) queen = true;
+                if (c.getValue().equals("K")) king = true;
+                if (c.getValue().equals("A")){
+                    total++;
+                }
+            }
+            else{
+                total += Integer.parseInt(c.getValue());
             }
         }
+        if (jack && queen && king && selected.size() == 3) return true;
+        else if (total == 11) return true;
+        return false;
     }
-    public void returnCard(Card c){
-        deck.add(c);
+
+    public boolean noPossibleCombinationsLeft(ArrayList<Card> cards) {
+        ArrayList<Card> temp = new ArrayList<Card>();
+        temp.addAll(cards);
+        boolean jack = false;
+        boolean queen = false;
+        boolean king = false;
+        for (int i = 0; i < temp.size(); i++) {
+            if (temp.get(i).getValue().equals("J") || temp.get(i).getValue().equals("Q") || temp.get(i).getValue().equals("K")) {
+                if (temp.get(i).getValue().equals("J")) jack = true;
+                if (temp.get(i).getValue().equals("Q")) queen = true;
+                if (temp.get(i).getValue().equals("K")) king = true;
+                temp.remove(i);
+                i--;
+            }
+        }
+        if (jack && queen && king) {
+            return false;
+        }
+        for (int i = 0; i < temp.size(); i++){
+            int currSum = 0;
+            if (temp.get(i).getValue().equals("A")) currSum++;
+            else currSum = Integer.parseInt(temp.get(i).getValue());
+            for (int j = i + 1; j < temp.size(); j++){
+                int add = 0;
+                if (temp.get(j).getValue().equals("A")) add = 1;
+                else{
+                    add = Integer.parseInt(temp.get(j).getValue());
+                }
+                if (currSum + add == 11) return false;
+            }
+        }
+        return true;
     }
 }
--- src/DrawPanel.java
+++ src/DrawPanel.java
@@ -14,6 +14,11 @@ class DrawPanel extends JPanel implements MouseListener {
     private Deck currentDeck;
     private Rectangle button;
     private Rectangle otherButton;
+    private boolean won = false;
+    private boolean lost = false;
+    private ArrayList<Card> selected = new ArrayList<Card>();
+    private ArrayList<Integer> positions = new ArrayList<Integer>();
+    private int count;
 
     public DrawPanel() {
         button = new Rectangle(75, 200, 160, 26);
@@ -22,6 +27,7 @@ class DrawPanel extends JPanel implements MouseListener {
         hand = Card.buildHand();
         currentDeck = new Deck(hand);
         deck = currentDeck.getDeck();
+        count = deck.size();
     }
 
     protected void paintComponent(Graphics g) {
@@ -46,49 +52,70 @@ class DrawPanel extends JPanel implements MouseListener {
         g.setFont(new Font("Courier New", Font.BOLD, 20));
         g.drawString("GET NEW CARDS", 77, 220);
         g.drawString("REPLACE CARDS", 277, 70);
+        g.drawString("CARDS LEFT: " + count, 10, 450);
+        if (won){
+            g.drawString("YOU WONNNNNNNNNNNNNNNNNNNNNNNNN!", 50, 400);
+        }
+        if (lost){
+            g.drawString("YOU LOST! NO POSSIBLE COMBOS LEFT", 50, 400);
+        }
         g.drawRect((int)button.getX(), (int)button.getY(), (int)button.getWidth(), (int)button.getHeight());
         g.drawRect((int)otherButton.getX(), (int)otherButton.getY(), (int)otherButton.getWidth(), (int)otherButton.getHeight());
+
     }
 
     public void mousePressed(MouseEvent e) {
-
         Point clicked = e.getPoint();
-
-        if (e.getButton() == 1) {
+        if (e.getButton() == 1 || e.getButton() == 3) {
             if (button.contains(clicked)) {
                 hand = Card.buildHand();
-                //update/create new deck based on new hand
                 deck = new Deck(hand).getDeck();
+                won = false;
+                lost = false;
             }
-
-            for (int i = 0; i < hand.size(); i++) {
+            for (int i = 0; i < hand.size(); i++){
                 Rectangle box = hand.get(i).getCardBox();
-                if (box.contains(clicked)) {
-                    hand.get(i).flipCard();
-                }
-            }
-        }
-
-        if (e.getButton() == 3) {
-            for (int i = 0; i < hand.size(); i++) {
-                Rectangle box = hand.get(i).getCardBox();
-                boolean highlighted = hand.get(i).getHighlight();
-                if (box.contains(clicked) && highlighted){
-                    Card replaced = hand.get(i);
-                    hand.remove(i);
-                    hand.add(i, currentDeck.getNewCard());
-                    replaced.flipHighlight();
-                    currentDeck.returnCard(replaced);
-                }
-                else if (box.contains(clicked)) {
+                if (box.contains(clicked)){
                     hand.get(i).flipHighlight();
+                    boolean currHighlight = hand.get(i).getHighlight();
+                    if (currHighlight){
+                        selected.add(hand.get(i));
+                        positions.add(i);
+                    }
+                    else{
+                        Integer x = i;
+                        selected.remove(hand.get(i));
+                        positions.remove(positions.indexOf(x));
+                    }
+                }
+            }
+
+            if (otherButton.contains(clicked)){
+                if (currentDeck.canBeReplaced(selected)){
+                    for (int i = 0; i < selected.size(); i++){
+                        Card replaced = selected.get(i);
+                        int position = positions.get(i);
+                        Card c = currentDeck.getNewCard();
+                        hand.remove(position);
+                        hand.add(position, c);
+                        replaced.flipHighlight();
+                    }
+                    resetSelected();
+                    resetPositions();
                 }
             }
         }
-
-
+        count = deck.size();
+        if (count == 0) won = true;
+        if (currentDeck.noPossibleCombinationsLeft(hand)) lost = true;
     }
 
+    public void resetSelected(){
+        selected = new ArrayList<Card>();
+    }
+    public void resetPositions(){
+        positions = new ArrayList<Integer>();
+    }
     public void mouseReleased(MouseEvent e) { }
     public void mouseEntered(MouseEvent e) { }
     public void mouseExited(MouseEvent e) { }
